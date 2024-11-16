9209b51d946e7628b80c2686f954eb0abad3729b
Motyak <m0tyak_@live.fr>
2024-11-16 19:34:20 +0100

    static libs order in linking cmd matter WTF

--- monlang-LV2/src/test/playground.cpp
+++ monlang-LV2/src/test/playground.cpp
@@ -1,7 +1,7 @@
-// #include <utils/tommystring.h>
-// #include <montree/montree.h>
+#include <utils/tommystring.h>
+#include <montree/montree.h>
 #include <catch2/catch_amalgamated.hpp>
 
-// TEST_CASE ("fds") {
-//     REQUIRE (true);
-// }
+TEST_CASE ("fds") {
+    REQUIRE (true);
+}
