9866616332270ce2e30beaa051be37c7089747cf
Motyak <m0tyak_@live.fr>
2025-07-19 22:00:44 +0200

    added env_get in utils

--- common/utils/env-utils.h
+++ common/utils/env-utils.h
@@ -12,4 +12,12 @@ inline std::string env_or_default(const std::string& var_name, const std::string
     return var_str != ""? var_str : default_val;
 }
 
+inline std::optional<std::string> env_get(const std::string& var_name) {
+    auto var = std::getenv(var_name.c_str());
+    if (!var) {
+        return std::nullopt;
+    }
+    return std::string(var);
+}
+
 #endif // ENV_UTILS_H
