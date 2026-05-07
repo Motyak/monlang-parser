7c31e693c8c9865eeea6ee29a02f5eb833ca5446
Motyak <m0tyak_@live.fr>
2026-05-07 21:28:17 +0200

    fix core dumped

--- monlang-LV2/src/expr/BlockExpression.cpp
+++ monlang-LV2/src/expr/BlockExpression.cpp
@@ -27,7 +27,7 @@ MayFail<MayFail_<BlockExpression>> buildBlockExpression(const Word& word) {
         // handle oneline block expr, to adapt token_len accordingly
         if (__first_it && cbg.term && std::holds_alternative<MayFail_<ExpressionStatement>*>(statement.val)) {
             auto exprStmt = std::get<MayFail_<ExpressionStatement>*>(statement.val);
-            if (exprStmt->expression) {
+            if (exprStmt->expression && !exprStmt->expression->has_error()) {
                 set_token_len(exprStmt->expression->val, token_len(exprStmt->expression->val) + 1);
             }
         }
